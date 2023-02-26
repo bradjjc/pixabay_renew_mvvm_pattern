6c4015b711d842a4e62f9938453727a8e5d984f5
Jichang Jung <jichangjung@yahoo.com>
2023-02-27 01:09:54 +0900

    viewMdel 과 테스트코드 inplements 로 테스트와 메인을 같이 사용

--- lib/data/pixabay_api.dart
+++ lib/data/pixabay_api.dart
@@ -1,11 +1,13 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:pixabay_renew_mvvm_pattern/data/photo_api_repository.dart';
 import 'package:pixabay_renew_mvvm_pattern/model/photo.dart';
 
-class PixabayApi {
+class PixabayApi implements PhotoApiRepasitory {
   final baseUrl = 'https://pixabay.com/api/';
   final key = '33944449-869c2910b825e571ac4fba36f';
 
+  @override
   Future<List<Photo>> fetch(String quary) async {
     String serchText = quary.replaceAll(' ', '+');
     final response = await http.get(
